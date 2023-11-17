63a0a2fd0be145cf9dbc7a35decc1846bef64ee1
Z-bot-pi <xulubanazintle@gmail.com>
2023-11-17 07:29:42 -0800

    p_notprintable.c

---
+++ p_reverse.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+* p_reverse - Prints reverse string.
+* @dataTypes: Lista of arguments
+* @buff: Buffer array to handle print
+* @flags: Calculates active flags
+* @width: get width
+* @bits: Precision specification
+* @size: Size specifier
+* Return: Numbers of chars printed
+*/
+int p_reverse(va_list dataTypes, char buff[],
+int flags, int width, int bits, int size)
+{
+char *str;
+int i, count = 0;
+UNUSED(buff);
+UNUSED(flags);
+UNUSED(width);
+UNUSED(size);
+str = va_arg(dataTypes, char *);
+if (str == NULL)
+{
+UNUSED(bits);
+str = ")Null(";
+}
+for (i = 0; str[i]; i++)
+;
+for (i = i - 1; i >= 0; i--)
+{
+char z = str[i];
+write(1, &z, 1);
+count++;
+}
+return (count);
+}
